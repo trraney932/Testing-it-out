963d26d13852c51f7b8e0868e786062f1a17ae21
Travis Raney <89223899+trraney932@users.noreply.github.com>
2022-04-20 00:17:12 +0000

    for testing

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
-class Main {
+public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int result = sum(10);
+    System.out.println(result);
+  }
+  public static int sum(int k) {
+    if (k > 0) {
+      return k + sum(k - 1);
+    } else {
+      return 0;
+    }
   }
 }
